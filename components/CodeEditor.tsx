cc7cb4b91622f3a587bfdf5f1037408ee2a2be21
syndg <sinchandsgpt9@gmail.com>
2024-03-02 20:12:29 +0530

    feat: add judge0 for remote code execution

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -2,24 +2,52 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
+import axios from "axios";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 const CodeEditor = () => {
   const [code, setCode] = useState("");
+  const [token, setToken] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [output, setOutput] = useState("");
 
   const handleChange = React.useCallback((value: string) => {
     setCode(value);
   }, []);
-  
+
+  useEffect(() => {
+    if (token !== "") {
+      getResult();
+    }
+  }, [token]);
+
+  const handleRun = async () => {
+    setIsLoading(true);
+    const tokenRes = await axios.post("/api/judge0", { code });
+    setToken(tokenRes.data.token);
+  };
+
+  const getResult = async () => {
+    const res = await axios.get(`/api/judge0/?token=${token}`);
+    console.log(atob(res.data?.stdout));
+    setIsLoading(false);
+  };
+
   return (
-    <CodeMirror
-      value={code}
-      height="100vh"
-      theme={vscodeDark}
-      extensions={[javascript()]}
-      onChange={handleChange}
-    />
+    <div>
+      <CodeMirror
+        value={code}
+        height="100vh"
+        theme={vscodeDark}
+        extensions={[javascript()]}
+        onChange={handleChange}
+      />
+      <Button onClick={handleRun}>Run</Button>
+      <div>{isLoading ? "Loading..." : ""}</div>
+      <span>{output}</span>
+    </div>
   );
 };
 
