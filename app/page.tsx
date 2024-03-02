a028c0d367438de2bd689eb8434e894119e4b86d
Pritika-Shukla <pritikashukla21@gmail.com>
2024-03-02 18:17:54 +0530

    feat: added macbook component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { TypewriterEffectSmoothDemo } from "@/components/ui/typewriter/typewriter";
 import Main from "@/components/main";
+import Navbar from "@/components/navbar";
+import { MacbookScrollDemo } from "@/components/ui/macBook/macBook";
 export default function Home() {
   return (
     <div className="min-h-screen w-screen bg-[#ffffff] flex flex-col    ">
-      <TypewriterEffectSmoothDemo />
+      <Navbar/>
       <Main />
     </div>
   );
--- components/main.tsx
+++ components/main.tsx
@@ -19,7 +19,7 @@ export function Main() {
           <button className="p-[3px] relative ">
             <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
             <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-              sart meeting
+              start meeting
             </div>
           </button>
           <button className="p-[3px] relative ">
