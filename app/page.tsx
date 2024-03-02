935f015d82a621666f52e2bd0b3384fd63d59235
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-03-02 13:43:47 +0530

    update: folder structure improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-
-import LandingPage from "./page/landingPage";
+import { TypewriterEffectSmoothDemo } from "@/components/ui/typewriter/typewriter";
+import Main from "@/components/main";
 export default function Home() {
   return (
-   <>
-   <LandingPage/>
-
-   </>
+    <div className="min-h-screen w-screen bg-[#ffffff] flex flex-col    ">
+      <TypewriterEffectSmoothDemo />
+      <Main />
+    </div>
   );
-
 }
