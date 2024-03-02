1f275870a2cc87ac8c4e13246df1bb6904735563
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2024-03-02 20:28:34 +0530

    fix: fixed some minor changes (#8)
    * fix: to be fixed
    
    * feat: added card in landing page
    
    * feat: fix minor things

--- components/feature-card.tsx
+++ components/feature-card.tsx
@@ -1,3 +1,8 @@
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "100",
+});
 type FeatureCardProps = {
   icon: any;
   title: string;
@@ -5,13 +10,19 @@ type FeatureCardProps = {
 };
 const FeatureCard = ({ icon, title, description }: FeatureCardProps) => {
   return (
-    <div className="flex bg-[#111111] max-w-[370px] px-4 py-6 rounded-md  border-[1px] border-solid border-purple-500 flex-col cursor-pointer shadow-md transition-all hover:shadow-purple-400">
+    <div
+      className={`flex bg-[#111111] max-w-[370px] px-4 py-6 rounded-md  border-[1px] border-solid border-purple-500 flex-col cursor-pointer shadow-md transition-all hover:shadow-purple-400 `}
+    >
       {icon}
       <div className="flex flex-col mt-9">
-        <h2 className="text-3xl text-gray-300 border-b-[1px] border-solid border-gray-700 pb-3 mb-4">
+        <h2
+          className={`text-3xl text-gray-300 border-b-[1px] border-solid border-gray-700 pb-3 mb-4 `}
+        >
           {title}
         </h2>
-        <h5 className="text-xl text-gray-400 mt-5">{description}</h5>
+        <h5 className={`text-xl text-gray-200 mt-5 tracking-wide`}>
+          {description}
+        </h5>
       </div>
     </div>
   );
--- components/ui/macBook/macbook-scroll.tsx
+++ components/ui/macBook/macbook-scroll.tsx
@@ -68,8 +68,7 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-
-      className="min-h-[200vh]  flex flex-col items-center py-0 md:py-10 justify-start flex-shrink-0 [perspective:800px] transform md:scale-100  scale-[0.35] sm:scale-50"
+      className="min-h-[200vh]  flex flex-col items-center py-0 md:py-80 justify-start flex-shrink-0 [perspective:800px] transform md:scale-100  scale-[0.35] sm:scale-50"
     >
       <motion.h2
         style={{
